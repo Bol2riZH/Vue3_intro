4621812a2b5349df2b54e2e1febdf8a247bf210b
Matthieu <reungoat.matthieu@gmail.com>
2022-12-08 11:45:07 +0100

    lesson 9 done

---
+++ L9/components/ProductDetails.js
@@ -0,0 +1,9 @@
+app.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: /*html*/ `<ul><li v-for="detail in details">{{ detail }}</li></ul>`,
+});
